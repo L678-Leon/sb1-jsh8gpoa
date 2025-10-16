e9a42cad4a8804f1943329848614b253198898a4
L678-Leon <leonardito985@gmail.com>
2025-10-16 14:45:52 +0300

    Updated ProductsPage.tsx

--- src/pages/ProductsPage.tsx
+++ src/pages/ProductsPage.tsx
@@ -26,7 +26,7 @@ const categoryDescriptions: { [key: string]: string } = {
 const categoryImages: { [key: string]: string } = {
   'WEIGHBRIDGES': '/images/WhatsApp Image 2025-09-30 at 21.31.21_992705e9.jpg',
   'INDUSTRIAL SCALES': '/images/WhatsApp Image industrial scales.jpg',
-  'RETAIL SCALES': '/images/Screenshot_26-8-2025_1051_.jpeg',
+  'RETAIL SCALES': '/images/Screenshot_26-8-2025_2165_.jpeg',
   'FIELD SCALES': '/images/WhatsApp Image 2025-09-12 at 15.41.27_16846bac fs.jpg',
   'PRECISION SCALES': '/images/Screenshot_26-8-2025_141420_.jpeg',
   'INDICATORS': '/images/Screenshot_26-8-2025_21629_.jpeg',
